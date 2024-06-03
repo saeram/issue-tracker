0d9486dd8d20e8587b09e4281f075940d81758c8
saeram <seram1427@gmail.com>
2024-06-03 14:26:26 +0900

    Implement client-side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -7,15 +7,17 @@ import axios from 'axios';
 import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from 'zod';
 
-interface IssueForm {
-    title: string;
-    description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
     const router = useRouter();
-    const {register, control, handleSubmit} = useForm<IssueForm>();
+    const {register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
+      resolver: zodResolver(createIssueSchema)
+    });
     const [error, setError] = useState('');
 
   return (
@@ -36,11 +38,14 @@ const NewIssuePage = () => {
 })}>
 
       <TextField.Root placeholder="Title" {...register('title')}/>
+      {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
       <Controller
         name="description"
         control={control}
         render={({ field }) => <SimpleMDE placeholder="Description" {...field}/>}
         />
+        {errors.title && <Text color="red" as="p">{errors.description.message}</Text>}
+
       
       <Button>Submit New Issue</Button>
        
